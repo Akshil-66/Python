67dd81d06c7ed13202fc629d55eadbf3be20d2a5
Akshil_Nagani_19 <axilpatel149@gmail.com>
2026-08-02 01:08:05 +0530

    Add files via upload

---
+++ Project 3 - Text editor app/main.py
@@ -0,0 +1,87 @@
+# import tkinter for creating GUI apps
+# GUI = Graphical User Interface
+
+import tkinter as tk
+
+from tkinter import filedialog,messagebox # import sub module of tkinter
+
+# Main window code
+
+root = tk.Tk()
+root.title("Simple Text Editor")
+root.geometry("800x600")
+
+# create text area
+
+text = tk.Text(
+    root,
+    wrap=tk.WORD,
+    font=("Helvetica",12)
+)
+
+text.pack(expand=True,fill=tk.BOTH)
+
+# main logic starts now
+
+#Function 1 - to create a new file
+
+def new_file():
+    text.delete(1.0,tk.END)
+
+# Function 2 - to open a new file
+
+def open_file():
+
+    # open file dialogue
+    file_path=filedialog.askopenfilename(
+        defaultextension=".txt",
+        filetypes=[("Text Files" , "*.txt")]
+    )
+
+    if file_path:
+        #open selected file
+        with open(file_path,"r") as file:
+            text.delete(1.0,tk.END)
+            text.insert(tk.END,file.read())
+
+# Function 3 - save the file
+
+def save_file():
+    # open save file dialogue
+    file_path=filedialog.askopenfilename(
+         defaultextension=".txt",
+         filetypes=[("Text Files","*.txt")]
+    )
+
+    if file_path:
+        with open(file_path,"w") as file:
+            file.write(text.get(1.0,tk.END))
+
+    messagebox.showinfo("Info","File saved successfully")
+
+# Menubar
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+file_menu = tk.Menu(menu)
+
+# New , open , save , exit
+
+# add file menu to menu bar
+
+menu.add_cascade(label="File" , menu=file_menu)
+
+file_menu.add_command(label="New",command=new_file)
+file_menu.add_command(label="Open",command=open_file)
+file_menu.add_command(label="Save",command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit",command=root.quit)
+
+
+
+
+# starts and keeps the window open
+root.mainloop()
+
+
